18df10a5af7d3cd9629a879ccec1e4f3efb92396
AvatrixXYT <82200585+AvatrixXYT@users.noreply.github.com>
2021-09-03 23:35:11 -0500

    Add files via upload

---
+++ t2.cpp
@@ -0,0 +1,43 @@
+//Note:The template file will be copied to a new file. When you change the code of the template file you can create new file with this base code. 
+#include <iostream> 
+using namespace std;
+
+int main(){
+    
+    int opsion, n1, n2;
+    cout << "que quieres aser 1 suma 2 resta 3 multiplicasion 4 division \n";
+    cin >> opsion;
+    if (opsion == 1){
+        cout << "escribe el primer numero \n";
+        cin >> n1;
+        cout << "ahora el segundo \n";
+        cin >> n2;
+        cout << "el resultado es \n";
+        cout << n1 + n2;
+    }
+        if (opsion == 2){
+        cout << "escribe el primer numero \n";
+        cin >> n1;
+        cout << "ahora el segundo \n";
+        cin >> n2;
+        cout << "el resultado es \n";
+        cout << n1 - n2;
+    }
+            if (opsion == 3){
+        cout << "escribe el primer numero \n";
+        cin >> n1;
+        cout << "ahora el segundo \n";
+        cin >> n2;
+        cout << "el resultado es \n";
+        cout << n1 * n2;
+}
+        if (opsion == 4){
+        cout << "escribe el primer numero \n";
+        cin >> n1;
+        cout << "ahora el segundo \n";
+        cin >> n2;
+        cout << "el resultado es \n";
+        cout << n1 / n2;
+        }
+        return 0;
+}
