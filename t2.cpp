b42f2d7aaee8e7c633d49a81e9625e0ab8707370
AvatrixXYT <82200585+AvatrixXYT@users.noreply.github.com>
2021-09-03 23:38:07 -0500

    Update t2.cpp

--- t2.cpp
+++ t2.cpp
@@ -1,4 +1,3 @@
-//Note:The template file will be copied to a new file. When you change the code of the template file you can create new file with this base code. 
 #include <iostream> 
 using namespace std;
 
